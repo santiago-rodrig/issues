6d7cfa307ba6e2fd79a922977f870e4ec9bcd161
santiago-rodrig <santo1996.29@gmail.com>
2021-03-11 08:15:40 -0400

    program is complete

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/santiago-rodrig/itracker"
+)
+
 func main() {
-	println("not implemented")
+	result, err := itracker.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%d issues:\n", result.TotalCount)
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	}
 }
